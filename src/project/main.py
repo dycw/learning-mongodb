1745cb67800e8bccdc5693c079ceb35577fdcf07
Derek Wan <d.wan@icloud.com>
2023-05-02 11:43:24 +0800

    Add routes

--- src/project/main.py
+++ src/project/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from project import routes
 from dotenv import dotenv_values
 from loguru import logger
 from pymongo import MongoClient
@@ -11,12 +12,15 @@ app = FastAPI()
 
 
 @app.on_event("startup")
-def startup_db_client() -> None:
+async def startup_db_client() -> None:
     app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAMe"]]
+    app.database = app.mongodb_client[config["DB_NAME"]]
     logger.info("Connected to the MongoDB database!")
 
 
 @app.on_event("shutdown")
 async def root() -> None:
     app.mongodb_client.close()
+
+
+app.include_router(routes.router)
